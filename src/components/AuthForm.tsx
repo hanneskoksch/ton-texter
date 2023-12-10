6b87d95e2846a50777b0765cb5f24ebd6edc29aa
Ziegler Torben <tz023@hdm-stuttgart.de>
2023-12-10 19:18:25 +0100

    fix: Clear import

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -2,8 +2,7 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/../types/supabase' 
-import process from 'process';
+import { Database } from '@/../types/supabase'
 
 export default function AuthForm() {
   const supabase = createClientComponentClient<Database>()
