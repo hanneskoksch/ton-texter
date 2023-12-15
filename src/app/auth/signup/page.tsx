5d4eeba2fd2f281e06471b49d6fb899c57c49f08
Torben Ziegler <torbenziegler99@web.de>
2023-12-15 20:14:49 +0100

    feat: add email password login

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,9 +1,63 @@
+"use client";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
+import { useState } from "react";
+import { supabase } from "@/lib/supabaseClient";
+import { Button, Input } from "@nextui-org/react";
+
 export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignup = async (e: any) => {
+    e.preventDefault();
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+      if (error) {
+        setError(error.message);
+        return;
+      }
+      // Handle successful signup, e.g., redirect user to a new page
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+
   return (
     <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-      <p>TBD</p>
+      <form
+        onSubmit={handleSignup}
+        className="flex items-center justify-center"
+      >
+        <div className="flex flex-col">
+          <Input
+            type="email"
+            placeholder="E-Mail"
+            label="E-Mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="mb-4"
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            label="Passwort"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="mb-4"
+          />
+          <Button type="submit" color="default">
+            Registrieren
+          </Button>
+          {error && <p>{error}</p>}
+        </div>
+      </form>
     </MaxWidthWrapper>
   );
 }
