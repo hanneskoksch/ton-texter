d54846e5d67a7594959fb8fdd33c85db71c828ee
Torben Ziegler <torbenziegler99@web.de>
2023-12-28 17:20:55 +0100

    feat: Adapt markdown layout to dark mode

--- src/app/legal/layout.tsx
+++ src/app/legal/layout.tsx
@@ -1,4 +1,4 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     // Create any shared layout or styles here
-    return <div className="mx-auto max-w-screen-md px-4 mt-8 mb-20 prose lg:prose-x" style={{ }}>{children}</div>
+    return <div className="mx-auto max-w-screen-md px-4 mt-8 mb-20 prose dark:prose-invert lg:prose-x" style={{ }}>{children}</div>
   }
