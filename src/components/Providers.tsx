01e79e74deefb1068f73029aec33065400204a8d
Hannes Koksch <hannes.koksch@gmail.com>
2023-12-29 14:34:09 +0100

    added tRPC

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import * as React from "react";
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NextUIProvider } from "@nextui-org/react";
-import { PropsWithChildren } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { trpc } from "@/app/_trpc/client";
+import { httpBatchLink } from "@trpc/client";
+import { getURL } from "@/lib/utils";
 
 const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${getURL()}api/trpc`,
+        }),
+      ],
+    })
+  );
   return (
-    <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="light">
-        {children}
-      </NextThemesProvider>
-    </NextUIProvider>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <NextUIProvider>
+          <NextThemesProvider attribute="class" defaultTheme="light">
+            {children}
+          </NextThemesProvider>
+        </NextUIProvider>
+      </QueryClientProvider>
+    </trpc.Provider>
   );
 };
 
