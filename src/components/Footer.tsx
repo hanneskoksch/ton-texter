0577b0a315691319444c452868e8b05b1bdb6002
Torben Ziegler <torbenziegler99@web.de>
2024-01-04 18:26:35 +0100

    fix: Footer overlapping content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Divider, Link } from "@nextui-org/react";
 
 const Footer = () => {
   return (
-    <footer className="py-4 fixed bottom-0 left-0 w-full">
+    <footer className="py-4 mt-auto bottom-0 left-0 w-full">
       <Divider className="my-4" />
       <div className="flex justify-center text-sm mb-2">
         &copy; 2024 Ton-Texter
