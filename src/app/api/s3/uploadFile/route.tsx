fe4f42394ce44688ba4794c371e63b222ca9c167
Torben Ziegler <torbenziegler99@web.de>
2024-01-04 17:44:19 +0100

    fix: Remove log line

--- src/app/api/s3/uploadFile/route.tsx
+++ src/app/api/s3/uploadFile/route.tsx
@@ -61,7 +61,6 @@ export async function POST(req: NextRequest) {
     }
     return NextResponse.json({ success: true });
   } catch (error) {
-    console.log(error);
     return NextResponse.error();
   }
 }
