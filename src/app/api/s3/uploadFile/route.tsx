a8bdc5a9b0fbb0369d728643970ca7d9b28f980f
Hannes Koksch <hannes.koksch@gmail.com>
2024-01-05 19:19:00 +0100

    typo

--- src/app/api/s3/uploadFile/route.tsx
+++ src/app/api/s3/uploadFile/route.tsx
@@ -50,7 +50,7 @@ export async function POST(req: NextRequest) {
       const newTranscript = await db.transcript.create({
         data: {
           filename: fileNameWithUuid,
-          originalFilename: `${fileName}${fileExtension}}`,
+          originalFilename: `${fileName}${fileExtension}`,
           userId: userId,
         },
       });
