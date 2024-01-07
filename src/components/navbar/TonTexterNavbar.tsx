d5a22c83e567442839f4545f066f6de15ef8752e
Hannes Koksch <hannes.koksch@gmail.com>
2024-01-08 00:01:32 +0100

    mobile nav menu

--- src/components/navbar/TonTexterNavbar.tsx
+++ src/components/navbar/TonTexterNavbar.tsx
@@ -3,11 +3,14 @@ import {
   Navbar,
   NavbarBrand,
   NavbarContent,
+  NavbarMenuToggle,
   NavbarItem,
   Link as NextUILink,
+  NavbarMenu,
+  NavbarMenuItem,
 } from "@nextui-org/react";
 import Link from "next/link";
-import AuthButton from "./AuthButton";
+import AuthButton from "@/components/navbar/AuthButton";
 
 function TonTexterNavbar() {
   return (
@@ -18,18 +21,40 @@ function TonTexterNavbar() {
           <p className="text-lg font-bold text-inherit ">Ton-Texter</p>
         </Link>
       </NavbarBrand>
-      <NavbarContent className="gap-4 sm:flex" justify="start">
+      <NavbarContent className="hidden sm:flex gap-4" justify="start">
         <NavbarItem>
           <NextUILink href="/blog">Blog</NextUILink>
         </NavbarItem>
+        <NavbarItem>
+          <NextUILink href="/pricing">Pricing</NextUILink>
+        </NavbarItem>
       </NavbarContent>
       <NavbarContent className="gap-4 sm:flex" justify="end">
-        <NavbarItem>
+        <NavbarMenuToggle className="sm:hidden" />
+        <NavbarItem className="hidden sm:flex gap-4">
           <Suspense>
             <AuthButton />
           </Suspense>
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Suspense>
+            <AuthButton />
+          </Suspense>
+        </NavbarMenuItem>
+
+        <NavbarMenuItem>
+          <NextUILink className="w-full" size="lg" href="/pricing">
+            Pricing
+          </NextUILink>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <NextUILink className="w-full" size="lg" href="/blog">
+            Blog
+          </NextUILink>
+        </NavbarMenuItem>
+      </NavbarMenu>
     </Navbar>
   );
 }
