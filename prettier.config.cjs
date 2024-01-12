3f62d8f1fc0376d3c118cb95920e745065ff90e2
Hannes Koksch <hannes.koksch@gmail.com>
2024-01-12 15:40:42 +0100

    added prettier tailwind plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -8,6 +8,7 @@ const config = {
   semi: true,
   trailingComma: "all",
   tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
   //   proseWrap: "always", // printWidth line breaks in md/mdx
 };
 
