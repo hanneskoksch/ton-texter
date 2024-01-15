b990e16989deeaf2ba015beba05c1ce6eb73c6b7
Hannes Koksch <hannes.koksch@gmail.com>
2024-01-15 13:02:24 +0100

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <head />
+      <head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/tontexter_logo_small.svg"
+        />
+      </head>
       <body className={inter.className}>
         <Providers>
           <div className="flex min-h-screen flex-col">
