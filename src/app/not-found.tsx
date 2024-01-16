5d3adb40b010d6566b4debd16d352a78a74a2ec1
Hannes Koksch <hannes.koksch@gmail.com>
2024-01-16 14:17:01 +0100

    place footer below viewport in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 function NotFound() {
   return (
-    <div className="w-screen dark">
-      <div className="p-12 md:pt-44">
+    <div className="-mt-[4rem] h-screen w-screen dark">
+      <div className="p-12 pt-32 md:pt-44">
         <div className="text-center text-white">
           <h1 className="mb-6 text-9xl font-bold text-white">404</h1>
           <p className="mb-3 text-3xl">You&lsquo;ve come a long way, cowboy.</p>
