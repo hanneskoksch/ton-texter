08299b9fe5c20f49cd6c37a8cf32f14fd564fc1b
Hannes Koksch <hannes.koksch@gmail.com>
2024-01-16 14:19:16 +0100

    added black background layer for 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 
 function NotFound() {
   return (
-    <div className="-mt-[4rem] h-screen w-screen dark">
-      <div className="p-12 pt-32 md:pt-44">
+    <div className="relative -mt-[4rem] h-screen w-screen dark">
+      <div className="absolute inset-0 bg-black"></div>
+      <div className="relative z-10 p-12 pt-32 md:pt-44">
         <div className="text-center text-white">
           <h1 className="mb-6 text-9xl font-bold text-white">404</h1>
           <p className="mb-3 text-3xl">You&lsquo;ve come a long way, cowboy.</p>
@@ -28,7 +29,7 @@ function NotFound() {
         alt="404"
         layout="fill"
         objectFit="cover"
-        className="-z-10 opacity-30 md:opacity-100"
+        className="absolute z-0 opacity-30 md:opacity-100"
         objectPosition="left"
       />
     </div>
