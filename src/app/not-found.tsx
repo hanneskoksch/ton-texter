2e4578cad1ec4ada816f46a7dc45b7a9c69c1f86
Hannes Koksch <hannes.koksch@gmail.com>
2024-01-17 14:34:29 +0100

    fix: overflow of image on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,35 +3,39 @@ import Image from "next/image";
 
 function NotFound() {
   return (
-    <div className="relative -mt-[4rem] h-screen w-screen dark">
-      <div className="absolute inset-0 bg-black"></div>
-      <div className="relative z-10 p-12 pt-32 md:pt-44">
-        <div className="text-center text-white">
-          <h1 className="mb-6 text-9xl font-bold text-white">404</h1>
-          <p className="mb-3 text-3xl">You&lsquo;ve come a long way, cowboy.</p>
-          <p className="mb-6">
-            There is nothing else anymore, you reached the end.
-          </p>
-          <Button
-            color="default"
-            variant="bordered"
-            className="border-white"
-            as={Link}
-            href="/"
-          >
-            Go back home
-          </Button>
+    <div className="overflow-clip">
+      <div className="relative -mt-[4rem] h-screen w-screen">
+        <div className="absolute inset-0 bg-black"></div>
+        <div className="relative z-10 p-12 pt-32 md:pt-44">
+          <div className="text-center text-white">
+            <h1 className="mb-6 text-9xl font-bold text-white">404</h1>
+            <p className="mb-3 text-3xl">
+              You&lsquo;ve come a long way, cowboy.
+            </p>
+            <p className="mb-6">
+              There is nothing else anymore, you reached the end.
+            </p>
+            <Button
+              color="default"
+              variant="bordered"
+              className="border-white"
+              as={Link}
+              href="/"
+            >
+              Go back home
+            </Button>
+          </div>
         </div>
-      </div>
 
-      <Image
-        src="/images/space_cowboy_404.avif"
-        alt="404"
-        layout="fill"
-        objectFit="cover"
-        className="absolute z-0 opacity-30 md:opacity-100"
-        objectPosition="left"
-      />
+        <Image
+          src="/images/space_cowboy_404.avif"
+          alt="404"
+          layout="fill"
+          objectFit="cover"
+          className="absolute z-0 opacity-30 md:opacity-100"
+          objectPosition="left"
+        />
+      </div>
     </div>
   );
 }
