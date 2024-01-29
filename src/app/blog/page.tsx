d006d10c624c12f5283480d1454ef642717b626b
Hannes Koksch <hannes.koksch@gmail.com>
2024-01-29 15:19:26 +0100

    show blog post description

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from "@nextui-org/react";
 import { format } from "date-fns";
 import { Metadata } from "next";
 import Link from "next/link";
@@ -31,17 +32,26 @@ export default function BlogPage() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="mb-4 flex flex-col space-y-1"
+            className="mb-4 flex flex-col rounded-lg border-2 border-solid border-default-100 bg-default-50 p-2 hover:bg-background"
             href={`/blog/${post.slug}`}
           >
-            <div className="flex w-full flex-col">
+            <div className="flex justify-between space-x-2">
               <p className="tracking-tight text-neutral-900 dark:text-neutral-100">
                 {post.metadata.title}
               </p>
-              <p className="text-default-500">
-                {format(new Date(post.metadata.publishedAt), "dd.MM.yyyy")} •
-                von {post.metadata.author}
+              <p className="max-w-fit text-default-500">
+                {format(new Date(post.metadata.publishedAt), "dd.MM.yyyy")}
               </p>
+            </div>
+
+            <p className="text-default-500">{post.metadata.description}</p>
+            <div className="flex items-center space-x-2">
+              <Avatar
+                className="h-5 w-5"
+                name={post.metadata.author}
+                src={`https://github.com/${post.metadata.authorGithub}.png?size=50`}
+              />
+              <p className="text-default-500">{post.metadata.author}</p>
             </div>
           </Link>
         ))}
