b336d436fb8bc4524b027230ddb4f50b6d2eed39
Hannes Koksch <hannes.koksch@gmail.com>
2024-02-02 11:44:03 +0100

    rename env var since it is only used server-side

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -14,8 +14,8 @@ export async function GET(request: NextRequest) {
   if (token_hash && type) {
     const cookieStore = cookies();
     const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_ANON_KEY!,
       {
         cookies: {
           get(name: string) {
