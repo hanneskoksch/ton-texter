4b64f0e64bd1cfebe69e5f2a6ffd56a701fb1fc0
Hannes Koksch <hannes.koksch@gmail.com>
2024-02-05 20:52:06 +0100

    feat: provide blog metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,15 @@ export const metadata: Metadata = {
   description:
     "Navigiere spielend durch deine Inhalte und finde genau das, was du suchst – dank Ton-Texter Transkriptionen. Vereinfache deine Arbeitsweise und behalte stets den Überblick über deine Videoprojekte.",
   robots: "noindex,nofollow",
+  openGraph: {
+    images: [
+      {
+        url: "http://localhost:3000/ton-texter-og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+  }
 };
 
 export default function RootLayout({
