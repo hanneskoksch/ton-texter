2483452d93345bda4095e42a3e4b41cd5c88b60c
Hannes Koksch <hannes.koksch@gmail.com>
2025-02-08 21:49:08 +0100

    remove unused action

--- src/components/navbar/actions.ts
+++ src/components/navbar/actions.ts
@@ -6,8 +6,3 @@ export const logOut = async () => {
   const supabase = await createClient();
   await supabase.auth.signOut();
 };
-
-export const getUser = async () => {
-  const supabase = await createClient();
-  return await supabase.auth.getUser();
-};
