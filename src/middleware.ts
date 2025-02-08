89742a3cf6004b647fec4e4dca4111935e91f09c
Hannes Koksch <hannes.koksch@gmail.com>
2025-02-08 23:11:46 +0100

    invert middleware matcher logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,15 +7,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public assets (images, etc)
-     * - public routes (/blog/*, /pricing, /legal/*)
-     */
-    "/((?!_next/static|_next/image|favicon.ico|blog(?:/.*)?$|pricing$|legal(?:/.*)?$|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+  matcher: ["/dashboard/:path", "/api/trpc/:path",],
 };
